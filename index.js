b95188e604cd945f2fea9c7456678fe2359e325f
Sabrina Aileen <115700137+sabrinaaileen@users.noreply.github.com>
2025-08-07 15:05:18 +0200

    Added the API integration

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const generatePoem = (event) => {
-    event.preventDefault();
-
+const displayPoem = (response) => {
+    // display the poem
     new Typewriter('#poem-text', {
-        strings: "Die Nacht ist kalt <br /> doch kälter noch im Wald",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
     });
 }
 
+const generatePoem = (event) => {
+    event.preventDefault();
+    let userInstructions = document.querySelector("#user-instructions");
+    // build the api url
+    let apiKey = "02060cacd430ctof7d20b656741fc18d";
+    let prompt = `Create a poem in German which is about ${userInstructions.value}.`;
+    let context = "You are a poet and love to write short poems in 4 lines. Behave and be polite. Make a line break after each sentence with a <br />. And display the author <b>SheCodes AI</b> in bold at the end of the poem";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    // make the api call
+    axios.get(apiUrl).then(displayPoem);
+
+    console.log("Generating poem");
+
+    
+}
+
 
 const inputForm = document.querySelector("#input-form");
 inputForm.addEventListener("submit", generatePoem);
+
+//Die Nacht ist kalt <br /> doch kälter noch im Wald
